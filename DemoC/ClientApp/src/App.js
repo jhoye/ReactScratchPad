dbc10ff06cde7dc86e6b36e3e0e863cec04e652d
Jon Hoye <hoye.jon@gmail.com>
2020-10-28 09:24:41 -0500

    WIP
    - CMD: ReactScratchPad/DemoC>yarn add typeface-roboto

--- DemoC/ClientApp/src/App.js
+++ DemoC/ClientApp/src/App.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import 'typeface-roboto';
+import { Container, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import BookIcon from '@material-ui/icons/Book';
+import CategoryIcon from '@material-ui/icons/Category';
+import './App.css';
+
+const useStyles = makeStyles({
+    root: {
+        height: '100%'
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        width: '100%'
+    }
+});
 
 function App() {
+
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
     return (
-        <div>Material UI Demo</div>
+        <Container className={classes.root}>
+            <Typography
+                variant="h2"
+                gutterBottom>Material UI Demo</Typography>
+            <div>Material UI Demo</div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.</p>
+            <BottomNavigation
+                className={classes.footer}
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+                showLabels>
+                <BottomNavigationAction label="GTD" icon={<FlashOnIcon />} />
+                <BottomNavigationAction label="Memex" icon={<BookIcon />} />
+                <BottomNavigationAction label="Meta" icon={<CategoryIcon />} />
+            </BottomNavigation>
+        </Container>
     );
 }
 
-export default App; 
+export default App;
